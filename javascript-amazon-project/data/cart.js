f5973bae31bd3b480fcd0b9eb50ea68764b42bb1
Matthewdo <matthewdoarda@gmail.com>
2024-03-04 07:21:36 -0800

    import-export, cart funtions contained

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,1 +1,32 @@
-let cart = [];
+export let cart = [];
+
+export function cartFunc(button){
+    button.addEventListener('click',()=>{
+        const productName = button.dataset.productName;
+        const quantityValue = document.getElementById(`js-quantity-${productName}`).value;
+        let matchingItem;
+        let cartTotal = 0;
+        cart.forEach((item) => {
+          if(productName === item.productName){
+            matchingItem = item;
+          }
+         })
+          if(matchingItem){
+            matchingItem.quantity += Number(quantityValue);
+          }
+          else{
+            cart.push({
+              productName: productName,
+              quantity: Number(quantityValue)
+            })
+          }
+          cart.forEach((item) => {
+            cartTotal += item.quantity;
+           })
+    
+        document.querySelector('.js-cart-quantity')
+        .innerHTML = cartTotal;
+    
+        console.log(cart);
+      })
+}
--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,3 +1,6 @@
+import {cartFunc} from "../data/cart.js"
+import {products} from "../data/products.js"
+
 let productsHtml ='';
 
 products.forEach((product) => {
@@ -52,42 +55,16 @@ products.forEach((product) => {
  </div>`
 })
 
-
 document.querySelector('.js-product-grid')
 .innerHTML = productsHtml;
 
+
 document.querySelectorAll('.js-add-to-cart-button')
 .forEach((button) => {
-  button.addEventListener('click',()=>{
-    const productName = button.dataset.productName;
-    const quantityValue = document.getElementById(`js-quantity-${productName}`).value;
-    let matchingItem;
-    let cartTotal = 0;
-
-    cart.forEach((item) => {
-      if(productName === item.productName){
-        matchingItem = item;
-      }
-     })
-      if(matchingItem){
-        matchingItem.quantity += Number(quantityValue);
-      }
-      else{
-        cart.push({
-          productName: productName,
-          quantity: Number(quantityValue)
-        })
-      }
-      cart.forEach((item) => {
-        cartTotal += item.quantity;
-       })
-
-    document.querySelector('.js-cart-quantity')
-    .innerHTML = cartTotal;
-
-    console.log(cart);
-  })
+    cartFunc(button);
 })
+
+
 
 
 
